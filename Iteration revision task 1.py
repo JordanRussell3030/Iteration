7ba2a0f676eb57b745f465b596b4ac01e7be88ba
Jordan <Jordan@192.168.1.95>
2014-10-13 19:28:17 +0100

    Iteration revision tasks.

---
+++ Iteration revision task 1.py
@@ -0,0 +1,7 @@
+#Jordan Russell
+#13/10/14
+#Iteration revision task 1
+
+for count in range(1,11):
+    print("Hello World.")
+    
---
+++ Iteration revision task 2.py
@@ -0,0 +1,8 @@
+#Jordan Russell
+#13/10/14
+#Iteration revision task 2
+
+message = input("Please input a message: ")
+amount_of_messages = int(input("Please input the amount of times you would like your message printed: "))
+for count in range(amount_of_messages):
+    print(message)
