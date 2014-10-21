c11a643318088dd03c456b240efbf41bf6c231cc
44240 <44240@PC004145.coll.lr.local>
2014-10-21 12:47:10 +0100

    Outputs stars in an inputted number of rows and columns.

---
+++ Iteration Development Task 2.py
@@ -0,0 +1,10 @@
+#Jordan Russell
+#21/10/14
+#Iteration development task 2
+
+stars_per_row = int(input("Please input how many stars you would like in your row: "))
+number_of_columns = int(input("Please input how many rows of stars you would like: "))
+star = "*" * stars_per_row
+for stars in range(number_of_columns):
+    print(star[0:stars_per_row])
+    
