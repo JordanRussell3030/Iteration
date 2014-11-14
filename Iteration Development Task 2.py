96a5386c84c0850a2433bd31e7ae9ece837b104c
44240 <44240@PC004145.coll.lr.local>
2014-11-14 13:53:22 +0000

    Updated.

--- Iteration Development Task 2.py
+++ Iteration Development Task 2.py
@@ -3,7 +3,7 @@
 #Iteration development task 2
 
 stars_per_row = int(input("Please input how many stars you would like in your row: "))
-number_of_columns = int(input("Please input how many rows of stars you would like: "))
+number_of_rows = int(input("Please input how many rows of stars you would like: "))
 star = "*" * stars_per_row
 for stars in range(number_of_columns):
     print(star[0:stars_per_row])
